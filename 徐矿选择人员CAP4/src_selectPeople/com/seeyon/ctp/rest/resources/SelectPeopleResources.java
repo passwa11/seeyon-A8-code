0737f54c4d1da5147dac5ffffd8bf5d9b1b4a272
supermaniszhou <741963634@qq.com>
2019-05-23 11:34:24 +0800

    过滤有数据的明细行

--- 徐矿选择人员CAP4/src_selectPeople/com/seeyon/ctp/rest/resources/SelectPeopleResources.java
+++ 徐矿选择人员CAP4/src_selectPeople/com/seeyon/ctp/rest/resources/SelectPeopleResources.java
@@ -14,6 +14,8 @@ import com.seeyon.cap4.form.service.CAP4FormManager;
 import com.seeyon.cap4.template.util.CAPFormUtil;
 import com.seeyon.ctp.common.AppContext;
 import com.seeyon.ctp.common.exceptions.BusinessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
@@ -27,6 +29,8 @@ import java.util.stream.Collectors;
 @Consumes({"application/json"})
 @Produces({"application/json"})
 public class SelectPeopleResources extends BaseResource {
+
+    private Logger logger = LoggerFactory.getLogger(SelectPeopleResources.class);
 
     private static CAP4FormManager cap4FormManager = (CAP4FormManager) AppContext.getBean("cap4FormManager");
 
@@ -57,7 +61,7 @@ public class SelectPeopleResources extends BaseResource {
             List<FormDataSubBean> subBeans = CAP4FormKit.getSubBeans(cacheFormData);
 
             //过滤明细行，排除有数据的明细行
-            List<FormDataSubBean> excludeExist=new ArrayList<>();
+            List<FormDataSubBean> excludeExist = new ArrayList<>();
             for (int i = 0; i < subBeans.size(); i++) {
                 Map<String, Object> map = subBeans.get(i).getRowData();
                 for (String key : map.keySet()) {
@@ -82,7 +86,7 @@ public class SelectPeopleResources extends BaseResource {
                 Map<String, Object> masterMap = excludeExist.get(i).getRowData();
                 filldatas = new HashMap<>();
                 dataMap = new HashMap<>();
-                ZJsonObject zJsonObject=listJson.get(i);
+                ZJsonObject zJsonObject = listJson.get(i);
                 Map<String, Object> subTemp1 = new HashMap<>();
 
                 subTemp1.put("showValue", zJsonObject.getField0001());
@@ -115,7 +119,7 @@ public class SelectPeopleResources extends BaseResource {
                                 filldatas.put(key, subTemp3);
                             }
                             count++;
-                            dataMap.put( masterMap.get("id") + "", filldatas);
+                            dataMap.put(masterMap.get("id") + "", filldatas);
                         }
                         dataMap.put("recordId", masterMap.get("id") + "");
                     }
@@ -124,101 +128,28 @@ public class SelectPeopleResources extends BaseResource {
             }
             result.put("data", listMap);
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println("选择人员回填数据报错了：" + e.getMessage());
+            logger.error("选择人员回填数据报错了：" + e.getMessage());
         }
 
         return success(result);
     }
 
-//    @GET
-//    @Produces({"application/json"})
-//    @Path("backfillpeopleInfo")
-//    public Response backfillpeopleInfo(@QueryParam("masterId") String masterId,
-//                                       @QueryParam("value") String value,
-//                                       @QueryParam("subId") String recordId,
-//                                       @QueryParam("dataInfo") String dataInfo) throws BusinessException {
-//        //formBean是主表的信息
-//        FormBean formBean = cap4FormManager.getForm(Long.valueOf(formId), false);
-//        FormDataMasterBean cacheFormData = cap4FormManager.getSessioMasterDataBean(Long.valueOf(Long.parseLong(masterId)));
-//        String checkMaster=cap4FormManager.checkSessioMasterDataExists(masterId);
-//
-//        if (null == cacheFormData) {
-//            return fail("表单数据在session中找不到（masterId:" + masterId + "），请尝试重新打开。");
-//        }
-//        //subForms是明细表信息
-//        List<FormTableBean> subForms = formBean.getSubTableBean();
-//        FormTableBean subForm = subForms.get(0);
-//        //zlc ->这是获取明细表字段
-//        Map<String, FormFieldBean> fieldMap = subForm.getFieldMap4Name();
-//
-//        //fastjson 解析json字符串
-//        JSONObject jsonObject = JSON.parseObject(dataInfo);
-//        JSONArray jsonArray = jsonObject.getJSONArray("data");
-//        List<ZJsonObject> list = JSON.parseObject(jsonArray.toJSONString(), new TypeReference<List<ZJsonObject>>() {
-//        });
-//
-//        //我的想法：先将字段放进一个map集合中，值都为空
-//        List<String> fieldListKey = new ArrayList<>();
-//        for (Map.Entry entry : fieldMap.entrySet()) {
-//            fieldListKey.add((String) entry.getKey());
-//        }
-//
-//        List parentList = new ArrayList();
-//
-//        for (ZJsonObject zj : list) {
-//            Map showValue1 = new HashMap();
-//            showValue1.put("showValue", zj.getField0001());
-//            showValue1.put("showValue2", zj.getField0001());
-//            showValue1.put("value", zj.getField0001());
-//
-//            Map showValue2 = new HashMap();
-//            showValue2.put("showValue", zj.getField0003());
-//            showValue2.put("showValue2", zj.getField0003());
-//            showValue2.put("value", zj.getField0003());
-//
-//            Map showValue3 = new HashMap();
-//            showValue3.put("showValue", zj.getField0004());
-//            showValue3.put("showValue2", zj.getField0004());
-//            showValue3.put("value", zj.getField0004());
-//
-//            Map<String, Object> m = new HashMap<>();
-//            for (int i = 0; i < fieldListKey.size(); i++) {
-//                if (i == 0) {
-//                    m.put(fieldListKey.get(i), showValue1);
-//                }
-//                if (i == 1) {
-//                    m.put(fieldListKey.get(i), showValue2);
-//                }
-//                if (i == 2) {
-//                    m.put(fieldListKey.get(i), showValue3);
-//                }
-//
-//            }
-//            parentList.add(m);
-//        }
-//
-//
-//        List<FormDataSubBean> subs = cacheFormData.getSubData(subForm.getTableName());
-//        Map<String, Object> result = new HashMap<String, Object>();
-//
-//        result.put("subTbName", subForm.getTableName());
-//
-//        FormDataSubBean subdata = null;
-//////        // 赋值bug修改   新增到空白的一行上面
-////        List<FormDataSubBean> filterList = subs.stream().filter(item ->
-////                StrKit.isNull(CAP4FormKit.getFieldValue(item, SelectPeople.field0001.getText())))
-////                .collect(Collectors.toList());
-////
-////        if (StrKit.isNull(filterList)) {
-////            result.put("add", true);
-////            return success(result);
-////        }
-//
-//        Map<String, Object> masterMap = cacheFormData.getAllDataMap();
-//        subdata = subs.get(0);
-//        result.put("recordId", Long.toString(subdata.getId()));
-//        result.put("add", false);
-//
-//        return success(result);
-//    }
+    @GET
+    @Produces({"application/json"})
+    @Path("refreshCache")
+    public Response refreshCache(@QueryParam("masterId") String masterId) throws BusinessException {
+        //formBean是主表的信息
+        FormDataMasterBean cacheFormData = cap4FormManager.getSessioMasterDataBean(Long.valueOf(Long.parseLong(masterId)));
+        //明细行信息，使用cap4提供
+        try {
+            List<FormDataSubBean> subBeans = CAP4FormKit.getSubBeans(cacheFormData);
+            System.out.println(subBeans.size());
+        } catch (Exception e) {
+            System.out.println("刷新缓存出错了：" +e.getMessage());
+            logger.error("刷新缓存出错了：" + e.getMessage());
+        }
+        return success("");
+    }
+
 }
