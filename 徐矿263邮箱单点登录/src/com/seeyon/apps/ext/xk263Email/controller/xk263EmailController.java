c0017ca4db7deb538f261b5d25a8420b69cfb8b7
supermaniszhou <741963634@qq.com>
2019-05-29 16:38:31 +0800

    修改

--- 徐矿263邮箱单点登录/src/com/seeyon/apps/ext/xk263Email/controller/xk263EmailController.java
+++ 徐矿263邮箱单点登录/src/com/seeyon/apps/ext/xk263Email/controller/xk263EmailController.java
@@ -1,48 +1,40 @@
 package com.seeyon.apps.ext.xk263Email.controller;
 
-import com.seeyon.apps.ext.xk263Email.util.SSOMD5;
-import com.seeyon.apps.ext.xk263Email.util.SignUtil;
+import com.alibaba.fastjson.JSONObject;
+import com.seeyon.apps.ext.xk263Email.util.ZCommonUtil;
 import com.seeyon.ctp.common.controller.BaseController;
-import com.seeyon.v3x.common.web.login.CurrentUser;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class xk263EmailController extends BaseController {
 
-    /**
-     * 263授权单点登录sid和key
-     */
-    public static String SSO_CID = "ff80808150fbc5b2015124367cc103ba";
-    public static String SSO_KEY = "4fW3H8my2cBeE";
-    public static String MAIL_DOMAIN = "xkjt.net";
 
     public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        /**
-         * 获取单点登录URL
-         * 单点登录接口为HTTP链接形式，根据接口要求提供正确参数，
-         * 访问如下链接可以直接登录263 Web Mail，显示指定用户的邮箱
-         * http://pcc.263.net/PCC/263mail.do?cid=单点登录接口账号&domain=邮箱域名&uid=用户ID&sign=加密标识
-         * sign = 32位MD5 （ cid=单点登录接口账号&domain=邮箱域名&uid=用户ID&key=单点登录接口密钥 ）
-         * @return
-         */
-        String alias = CurrentUser.get().getLoginName();
+        String loginUrl = ZCommonUtil.get263LoginUrl();
+        String count = ZCommonUtil.getUnreadCount();
 
-        StringBuffer sb = new StringBuffer("http://pcc.263.net/PCC/263mail.do?");
-        sb.append("cid=");
-        sb.append(SSO_CID);
-        sb.append("&domain=");
-        sb.append(MAIL_DOMAIN);
-        sb.append("&uid=");
-        sb.append(alias);
-        sb.append("&sign=");
-        sb.append(SignUtil.sign("cid=" + SSO_CID, "&domain=" + MAIL_DOMAIN, "&uid=" + alias, "&key=" + SSO_KEY));
-
-        response.sendRedirect(sb.toString());
+        Map<String, Object> map = new HashMap<>();
+        map.put("code", 0);
+        map.put("url", loginUrl);
+        map.put("count", count);
+        JSONObject json = new JSONObject(map);
+        render(response, json.toJSONString());
         return null;
+    }
+
+    private void render(HttpServletResponse response, String text) {
+        response.setContentType("application/json;charset=UTF-8");
+        try {
+            response.setContentLength(text.getBytes("UTF-8").length);
+            response.getWriter().write(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
