6017ed20d516a25c879dc4bd214367b6600972d0
supermaniszhou <741963634@qq.com>
2019-06-20 11:57:23 +0800

    添加人员访问方法

--- 集团人员管理/src/com/seeyon/apps/ext/zMember/controller/zMemberController.java
+++ 集团人员管理/src/com/seeyon/apps/ext/zMember/controller/zMemberController.java
@@ -1,9 +1,25 @@
 package com.seeyon.apps.ext.zMember.controller;
 
 import com.seeyon.ctp.common.controller.BaseController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * 周刘成   2019/6/20
  */
 public class zMemberController extends BaseController {
+
+    /***
+     * 周刘成
+     * @param request
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    public ModelAndView listAll(HttpServletRequest request, HttpServletResponse response) throws Exception{
+        ModelAndView result = new ModelAndView("apps/organization/member/allMember");
+        return result;
+    }
 }
