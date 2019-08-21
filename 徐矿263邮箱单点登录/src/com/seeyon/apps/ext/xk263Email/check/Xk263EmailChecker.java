55df91c507b734e1048a503c9d4ddb329ed0b4d6
supermaniszhou <741963634@qq.com>
2019-08-21 14:27:09 +0800

    add

---
+++ 徐矿263邮箱单点登录/src/com/seeyon/apps/ext/xk263Email/check/Xk263EmailChecker.java
@@ -0,0 +1,13 @@
+package com.seeyon.apps.ext.xk263Email.check;
+
+import com.seeyon.ctp.menu.check.MenuCheck;
+
+/**
+ * 周刘成   2019-8-21
+ */
+public class Xk263EmailChecker implements MenuCheck {
+    @Override
+    public boolean check(long l, long l1) {
+        return true;
+    }
+}
