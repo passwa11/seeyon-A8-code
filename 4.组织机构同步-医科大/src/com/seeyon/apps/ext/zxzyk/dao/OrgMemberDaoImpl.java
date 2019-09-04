3d66268409cbe13c33f419bc5283f948cccc3166
supermaniszhou <741963634@qq.com>
2019-09-04 15:59:52 +0800

    update

--- 4.组织机构同步-医科大/src/com/seeyon/apps/ext/zxzyk/dao/OrgMemberDaoImpl.java
+++ 4.组织机构同步-医科大/src/com/seeyon/apps/ext/zxzyk/dao/OrgMemberDaoImpl.java
@@ -18,7 +18,7 @@ public class OrgMemberDaoImpl implements OrgMemberDao {
     public List<OrgMember> queryAddOrgMember() {
         String sql = "select DISTINCT c2.code,c2.name,c2.id,c2.POSTID,c2.description,c2.mobile,M_ORG_UNIT.id unitId,M_ORG_LEVEL.id levelId from " +
                 "(select memb.*,M_ORG_POST.id postid from (" +
-                "select * from V_ORG_MEMBER vm where not EXISTS(select * from M_ORG_MEMBER  where vm.code = M_ORG_MEMBER.code)) " +
+                "select * from V_ORG_MEMBER vm where not EXISTS(select * from M_ORG_MEMBER  where vm.code = M_ORG_MEMBER.code  and vm.name = M_ORG_MEMBER.name)) " +
                 "memb,M_ORG_POST  where memb.org_post_id = M_ORG_POST.code) c2 LEFT JOIN M_ORG_UNIT  on nvl(c2.org_account_id,c2.sup_department_id) = M_ORG_UNIT.code " +
                 "LEFT JOIN M_ORG_LEVEL  on c2.org_level_id=M_ORG_LEVEL.code";
 
