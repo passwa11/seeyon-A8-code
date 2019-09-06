5ef958185816e3ded42d56ff571ceec846a74f10
supermaniszhou <741963634@qq.com>
2019-09-06 14:33:56 +0800

    add

---
+++ 0.rest接口调用方法/Rest/src/com/test/RestTest.java
@@ -0,0 +1,88 @@
+package com.test;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 周刘成   2019-9-6
+ */
+public class RestTest {
+
+
+    public static String getToken() {
+        String token = "";
+        String url = "http://127.0.0.1:81/seeyon/rest/token";
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost httpPost = null;
+        HttpResponse response = null;
+        httpPost = new HttpPost(url);
+        httpPost.setHeader("Content-Type", "application/json;charset=utf-8");
+        String requestParams = "{\"userName\":\"gw\",\"password\":\"gw111111\"}";
+        StringEntity postingString = new StringEntity(requestParams, "utf-8");
+        httpPost.setEntity(postingString);
+        try {
+            response = client.execute(httpPost);
+            response.setHeader("Cache-Control", "no-cache");
+            System.out.println(response.getStatusLine().getStatusCode());
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                String resultString = EntityUtils.toString(response.getEntity(), "utf-8").replaceAll(" ", "");
+                JSONObject jsonObject = JSON.parseObject(resultString);
+                token = (String) jsonObject.get("id");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return token;
+    }
+
+    public static void testInterface() {
+        String token = getToken();
+        String urlpath = "http://127.0.0.1:81/seeyon/rest/affairs/pending/code/zhangsan?ticket=zhangsan&token=" + token;
+        URL url = null;
+        try {
+            url = new URL(urlpath);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "UTF-8"));
+            String line;
+            StringBuilder sb = new StringBuilder();
+            while ((line = br.readLine()) != null) {// 循环读取流
+                sb.append(line);
+            }
+            br.close();
+            connection.disconnect();
+            System.out.println(sb.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+        testInterface();
+    }
+}
