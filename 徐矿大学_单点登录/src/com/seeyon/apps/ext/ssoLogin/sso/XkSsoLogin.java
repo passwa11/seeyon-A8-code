2848861878c4a5a9b3774e9528ebf05fd35f7f32
supermaniszhou <741963634@qq.com>
2019-12-30 16:46:32 +0800

    update

--- 徐矿大学_单点登录/src/com/seeyon/apps/ext/ssoLogin/sso/XkSsoLogin.java
+++ 徐矿大学_单点登录/src/com/seeyon/apps/ext/ssoLogin/sso/XkSsoLogin.java
@@ -1,83 +1,30 @@
 package com.seeyon.apps.ext.ssoLogin.sso;
 
-import com.wiscom.is.IdentityFactory;
-import com.wiscom.is.IdentityManager;
+import com.seeyon.apps.ext.ssoLogin.util.StringHandle;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.net.URL;
-import java.net.URLDecoder;
-import java.util.Base64;
 
 /**
  * 周刘成   2019-12-16
  */
-public class XkSsoLogin extends HttpServlet {
+public class XkSsoLogin {
     private static Logger log = LoggerFactory.getLogger(XkSsoLogin.class);
 
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        PrintWriter pw = response.getWriter();
-        URL is_config = XkSsoLogin.class.getResource("/client.properties");
-        Cookie[] all_cookises = request.getCookies();
-        String decodeCookieValue = null;
-        IdentityFactory factory = null;
-        if (all_cookises != null) {
-            for (int i = 0; i < all_cookises.length; i++) {
-                Cookie myCookie = all_cookises[i];
-                if (myCookie.getName().equals("iPlanetDirectoryPro")) {
-                    decodeCookieValue = URLDecoder.decode(myCookie.getValue(), "GB2312");
-                    log.info("ticket：" + decodeCookieValue + "cookies 内容value是：" + myCookie.getValue());
-                }
+    public static void login(HttpServletRequest request, HttpServletResponse response) {
+
+        String loginName = request.getRemoteUser();
+        String encodeloginName = StringHandle.encode(loginName);
+        if (null != loginName) {
+            try {
+                response.sendRedirect("login/sso?from=xkSso&ticket=" + encodeloginName);
+            } catch (IOException e) {
+                log.error("单点登录OA系统出错了，错误信息：" + e.getMessage());
             }
         }
-        try {
-            factory = IdentityFactory.createFactory(is_config.getPath());
-        } catch (Exception var12) {
-            var12.printStackTrace();
-            pw.println("is_config.getPath() is error!");
-        }
-        IdentityManager im = factory.getIdentityManager();
-        String loginName = "";
-        if (decodeCookieValue == null) {
-            response.addHeader("SSOError", "no user login!");
-        } else {
-            loginName = im.getCurrentUser(decodeCookieValue);
-            char[] encodeStringCharArray = loginName.toCharArray();
-
-            for (int i = 0; i < encodeStringCharArray.length; ++i) {
-                ++encodeStringCharArray[i];
-            }
-
-            String ticket = new String(Base64.getEncoder().encode((new String(encodeStringCharArray)).getBytes()));
-            response.sendRedirect("login/sso?from=xkSso&ticket=" + ticket);
-        }
-
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
-
-//    public static void login(HttpServletRequest request, HttpServletResponse response) {
-//
-//        String loginName = request.getRemoteUser();
-//        String encodeloginName = StringHandle.encode(loginName);
-//        if (null != loginName) {
-//            try {
-//                response.sendRedirect("login/sso?from=xkSso&ticket=" + encodeloginName);
-//            } catch (IOException e) {
-//                log.error("单点登录OA系统出错了，错误信息：" + e.getMessage());
-//            }
-//        }
-//    }
 
 }
--- 徐矿大学_单点登录/src/com/seeyon/apps/ext/ssoLogin/sso/XkdxSsoLoginHandshake.java
+++ 徐矿大学_单点登录/src/com/seeyon/apps/ext/ssoLogin/sso/XkdxSsoLoginHandshake.java
@@ -1,8 +1,7 @@
 package com.seeyon.apps.ext.ssoLogin.sso;
 
+import com.seeyon.apps.ext.ssoLogin.util.StringHandle;
 import com.seeyon.ctp.portal.sso.SSOLoginHandshakeAbstract;
-
-import java.util.Base64;
 
 /**
  * 周刘成   2019-12-16
@@ -10,13 +9,13 @@ import java.util.Base64;
 public class XkdxSsoLoginHandshake extends SSOLoginHandshakeAbstract {
     @Override
     public String handshake(String ticket) {
-        String encodeString = new String(Base64.getDecoder().decode(ticket.getBytes()));
-        char[] charArray = encodeString.toCharArray();
-        for (int i = 0; i < charArray.length; ++i) {
-            --charArray[i];
+        String decodeloginName = "";
+        if (null != ticket && !"".equals(ticket)) {
+            decodeloginName = StringHandle.decode(ticket);
+        } else {
+            return null;
         }
-        String loginName = new String(charArray);
-        return loginName;
+        return decodeloginName;
     }
 
     @Override
