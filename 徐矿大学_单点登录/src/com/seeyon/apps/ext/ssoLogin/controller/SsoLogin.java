39c54c46d9679bd308ad7e0c569e84c392c33beb
supermaniszhou <741963634@qq.com>
2019-12-31 15:57:17 +0800

    update

--- 徐矿大学_单点登录/src/com/seeyon/apps/ext/ssoLogin/controller/SsoLogin.java
+++ 徐矿大学_单点登录/src/com/seeyon/apps/ext/ssoLogin/controller/SsoLogin.java
@@ -1,15 +1,17 @@
 package com.seeyon.apps.ext.ssoLogin.controller;
 
 import com.seeyon.apps.ext.ssoLogin.util.StringHandle;
+import org.jasig.cas.client.authentication.AttributePrincipal;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.security.Principal;
+import java.util.Map;
 
 /**
  * 周刘成   2019-12-31
@@ -24,7 +26,9 @@ public class SsoLogin extends HttpServlet {
         String encodeloginName = StringHandle.encode(loginName);
         if (null != loginName) {
             try {
-                response.sendRedirect("login/sso?from=xkSso&ticket=" + encodeloginName);
+                String servername = request.getServerName();
+                String url = "http://" + servername + "/seeyon/";
+                response.sendRedirect(url + "login/sso?from=xkSso&ticket=" + encodeloginName);
             } catch (IOException e) {
                 log.error("单点登录OA系统出错了，错误信息：" + e.getMessage());
             }
@@ -33,14 +37,6 @@ public class SsoLogin extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String loginName = request.getRemoteUser();
-        String encodeloginName = StringHandle.encode(loginName);
-        if (null != loginName) {
-            try {
-                response.sendRedirect("login/sso?from=xkSso&ticket=" + encodeloginName);
-            } catch (IOException e) {
-                log.error("单点登录OA系统出错了，错误信息：" + e.getMessage());
-            }
-        }
+        doPost(request, response);
     }
 }
