cfb8458e23f8977abf4640b4f964756bdf028100
supermaniszhou <741963634@qq.com>
2020-04-14 09:39:41 +0800

    update

--- 徐医附院_档案系统集成公文同步/src/com/seeyon/apps/ext/DTdocument/po/TemproraryEntity.java
+++ 徐医附院_档案系统集成公文同步/src/com/seeyon/apps/ext/DTdocument/po/TemproraryEntity.java
@@ -7,7 +7,14 @@ public class TemproraryEntity {
 
     private String id;
     private String subject;
+    private String doc_mark;
     private String issuer;
+    private String send_departmen;
+    private String pack_date;
+    private String status;
+    private String create_time;
+    private String year;
+    private String edoc_type;
 
     public String getId() {
         return id;
@@ -25,11 +32,67 @@ public class TemproraryEntity {
         this.subject = subject;
     }
 
+    public String getDoc_mark() {
+        return doc_mark;
+    }
+
+    public void setDoc_mark(String doc_mark) {
+        this.doc_mark = doc_mark;
+    }
+
     public String getIssuer() {
         return issuer;
     }
 
     public void setIssuer(String issuer) {
         this.issuer = issuer;
+    }
+
+    public String getSend_departmen() {
+        return send_departmen;
+    }
+
+    public void setSend_departmen(String send_departmen) {
+        this.send_departmen = send_departmen;
+    }
+
+    public String getPack_date() {
+        return pack_date;
+    }
+
+    public void setPack_date(String pack_date) {
+        this.pack_date = pack_date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(String create_time) {
+        this.create_time = create_time;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getEdoc_type() {
+        return edoc_type;
+    }
+
+    public void setEdoc_type(String edoc_type) {
+        this.edoc_type = edoc_type;
     }
 }
