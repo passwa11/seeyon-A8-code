1cf77ad0fcd7adc922bd9937a776a2414478dfff
supermaniszhou <741963634@qq.com>
2020-06-18 11:42:16 +0800

    update

--- 开发区_选择人员CAP4/seeyon/apps_res/cap/customCtrlResources/peopleResources/js/selectPeople.js
+++ 开发区_选择人员CAP4/seeyon/apps_res/cap/customCtrlResources/peopleResources/js/selectPeople.js
@@ -98,27 +98,46 @@
                         var newPeople = new Array();
                         for (var i = 0; i < peoples.data.length; i++) {
                             var a = arr[i];
-                            var flag = a.flag;
-                            if (flag == 'dzb') {
-                                var id = a.id;
-                                conditionId.push(id);
-                            } else {
-                                newPeople.push(a);
-                            }
+                            newPeople.push(a);
                         }
-                        if (conditionId.length == 0) {
-                            var jsonObj = {
-                                "masterId": content.contentDataId + "",
-                                "dataInfo": JSON.stringify(peoples),
-                                "flag": "0"
-                            };
+                        var jsonObj = {
+                            "masterId": content.contentDataId + "",
+                            "dataInfo": JSON.stringify(peoples),
+                            "flag": "0"
+                        };
 
+                        $.ajax({
+                            type: 'post',
+                            async: true,
+                            url: "/seeyon/rest/cap4/selectPeople/backfillpeopleInfo2",
+                            dataType: 'json',
+                            data: JSON.stringify(jsonObj),
+                            contentType: 'application/json',
+                            success: function (res) {
+                                // 判断是否需要添加
+                                if (res.code != 0) {
+                                    $.alert(res.message);
+                                    return;
+                                }
+                                var dataCount = res.data.dataCount + 1;
+                                addLineAndFilldata(content, adaptation, messageObj, privateId, peoples, dataCount);
+                            }, error: function (res) {
+                            }
+                        });
+
+                        function addLineAndFilldata(content, adaptation, messageObj, privateId, datainfo, flag) {
+                            flag--;
+                            var jsonAdd = {
+                                'masterId': content.contentDataId,
+                                'dataInfo': JSON.stringify(datainfo),
+                                'flag': flag + ""
+                            };
                             $.ajax({
                                 type: 'post',
                                 async: true,
                                 url: "/seeyon/rest/cap4/selectPeople/backfillpeopleInfo2",
                                 dataType: 'json',
-                                data: JSON.stringify(jsonObj),
+                                data: JSON.stringify(jsonAdd),
                                 contentType: 'application/json',
                                 success: function (res) {
                                     // 判断是否需要添加
@@ -126,162 +145,40 @@
                                         $.alert(res.message);
                                         return;
                                     }
+                                    var isNext = res.data.add;
+                                    var val = 0;
 
-                                    var dataCount = res.data.dataCount + 1;
-                                    addLineAndFilldata(content, adaptation, messageObj, privateId, peoples, dataCount);
+                                    if (isNext) {
+                                        var addLineParam = {};
+                                        var dataCount = res.data.dataCount;
 
-                                }, error: function (res) {
-                                }
-                            });
-
-                            function addLineAndFilldata(content, adaptation, messageObj, privateId, datainfo, flag) {
-                                flag--;
-                                var jsonAdd = {
-                                    'masterId': content.contentDataId,
-                                    'dataInfo': JSON.stringify(datainfo),
-                                    'flag': flag + ""
-                                };
-                                $.ajax({
-                                    type: 'post',
-                                    async: true,
-                                    url: "/seeyon/rest/cap4/selectPeople/backfillpeopleInfo2",
-                                    dataType: 'json',
-                                    data: JSON.stringify(jsonAdd),
-                                    contentType: 'application/json',
-                                    success: function (res) {
-                                        // 判断是否需要添加
-                                        if (res.code != 0) {
-                                            $.alert(res.message);
-                                            return;
+                                        addLineParam.tableName = res.data.tableName;
+                                        addLineParam.isFormRecords = true;
+                                        addLineParam.callbackFn = function () {
+                                            addLineAndFilldata(content, adaptation, messageObj, privateId, peoples, flag);
                                         }
-                                        var isNext = res.data.add;
-                                        var val = 0;
+                                        window.thirdPartyFormAPI.insertFormsonRecords(addLineParam);
+                                    } else {
+                                        var dataList = res.data.data;
+                                        if (null != dataList && dataList != '') {
+                                            for (var i = 0; i < dataList.length; i++) {
+                                                var backfill = {};
+                                                backfill.tableName = res.data.tableName;
+                                                backfill.tableCategory = "formson";
+                                                backfill.updateData = dataList[i][dataList[i].recordId];
+                                                backfill.updateRecordId = dataList[i].recordId;
+                                                adaptation.backfillFormControlData(backfill, privateId);
 
-                                        if (isNext) {
-                                            var addLineParam = {};
-                                            var dataCount = res.data.dataCount;
-
-                                            addLineParam.tableName = res.data.tableName;
-                                            addLineParam.isFormRecords = true;
-                                            addLineParam.callbackFn = function () {
-                                                addLineAndFilldata(content, adaptation, messageObj, privateId, peoples, flag);
                                             }
-                                            window.thirdPartyFormAPI.insertFormsonRecords(addLineParam);
-                                        } else {
-                                            var dataList = res.data.data;
-                                            if (null != dataList && dataList != '') {
-                                                for (var i = 0; i < dataList.length; i++) {
-                                                    var backfill = {};
-                                                    backfill.tableName = res.data.tableName;
-                                                    backfill.tableCategory = "formson";
-                                                    backfill.updateData = dataList[i][dataList[i].recordId];
-                                                    backfill.updateRecordId = dataList[i].recordId;
-                                                    adaptation.backfillFormControlData(backfill, privateId);
-
-                                                }
-                                            }
-
                                         }
 
                                     }
-                                });
-                            }
 
-                            dialog.close();
-                        } else {
-                            $.ajax({
-                                type: 'post',
-                                async: true,
-                                url: '/seeyon/ext/selectPeople.do?method=selectPeopleByDeskWorkId&id=' + conditionId,
-                                dataType: 'json',
-                                contentType: 'application/json',
-                                success: function (res) {
-                                    var c = newPeople.concat(res.data);
-                                    peoples['data'] = c;
-                                    var jsonObj = {
-                                        "masterId": content.contentDataId + "",
-                                        "dataInfo": JSON.stringify(peoples),
-                                        "flag": "0"
-                                    };
-
-                                    $.ajax({
-                                        type: 'post',
-                                        async: true,
-                                        url: "/seeyon/rest/cap4/selectPeople/backfillpeopleInfo2",
-                                        dataType: 'json',
-                                        data: JSON.stringify(jsonObj),
-                                        contentType: 'application/json',
-                                        success: function (res) {
-                                            // 判断是否需要添加
-                                            if (res.code != 0) {
-                                                $.alert(res.message);
-                                                return;
-                                            }
-
-                                            var dataCount = res.data.dataCount + 1;
-                                            addLineAndFilldata(content, adaptation, messageObj, privateId, peoples, dataCount);
-
-                                        }, error: function (res) {
-                                        }
-                                    });
-
-                                    function addLineAndFilldata(content, adaptation, messageObj, privateId, datainfo, flag) {
-                                        flag--;
-                                        var jsonAdd = {
-                                            'masterId': content.contentDataId,
-                                            'dataInfo': JSON.stringify(datainfo),
-                                            'flag': flag + ""
-                                        };
-                                        $.ajax({
-                                            type: 'post',
-                                            async: true,
-                                            url: "/seeyon/rest/cap4/selectPeople/backfillpeopleInfo2",
-                                            dataType: 'json',
-                                            data: JSON.stringify(jsonAdd),
-                                            contentType: 'application/json',
-                                            success: function (res) {
-                                                // 判断是否需要添加
-                                                if (res.code != 0) {
-                                                    $.alert(res.message);
-                                                    return;
-                                                }
-                                                var isNext = res.data.add;
-                                                var val = 0;
-
-                                                if (isNext) {
-                                                    var addLineParam = {};
-                                                    var dataCount = res.data.dataCount;
-
-                                                    addLineParam.tableName = res.data.tableName;
-                                                    addLineParam.isFormRecords = true;
-                                                    addLineParam.callbackFn = function () {
-                                                        addLineAndFilldata(content, adaptation, messageObj, privateId, peoples, flag);
-                                                    }
-                                                    window.thirdPartyFormAPI.insertFormsonRecords(addLineParam);
-                                                } else {
-                                                    var dataList = res.data.data;
-                                                    if (null != dataList && dataList != '') {
-                                                        for (var i = 0; i < dataList.length; i++) {
-                                                            var backfill = {};
-                                                            backfill.tableName = res.data.tableName;
-                                                            backfill.tableCategory = "formson";
-                                                            backfill.updateData = dataList[i][dataList[i].recordId];
-                                                            backfill.updateRecordId = dataList[i].recordId;
-                                                            adaptation.backfillFormControlData(backfill, privateId);
-
-                                                        }
-                                                    }
-
-                                                }
-
-                                            }
-                                        });
-                                    }
-
-                                    dialog.close();
                                 }
                             });
                         }
+
+                        dialog.close();
 
                     }
                 }, {
