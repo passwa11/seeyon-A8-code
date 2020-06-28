2fee90e55d2971afabb5fa1970f5d1b89a07d851
supermaniszhou <741963634@qq.com>
2020-06-28 10:55:19 +0800

    add

--- 徐矿_文单查看_20200624/src/com/seeyon/apps/ext/edocDetail/controller/edocDetailController.java
+++ 徐矿_文单查看_20200624/src/com/seeyon/apps/ext/edocDetail/controller/edocDetailController.java
@@ -1,14 +1,16 @@
 package com.seeyon.apps.ext.edocDetail.controller;
 
 import javax.servlet.http.HttpServletRequest;
+
 import com.seeyon.ctp.common.controller.BaseController;
+
 import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.web.servlet.ModelAndView;
 
-public class edocDetailController extends BaseController{
+public class edocDetailController extends BaseController {
 
-public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws Exception {
-/*JSP file path:ApacheJetspeed\webapps\seeyon\WEB-INF\jsp\apps\ext\edocDetail\edocDetail.jsp*/
-return new ModelAndView("apps/ext/edocDetail/index");
-}
+    public ModelAndView index(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return new ModelAndView("apps/ext/edocDetail/index");
+    }
 }
