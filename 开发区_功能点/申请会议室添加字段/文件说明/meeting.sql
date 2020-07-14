2ad3909fe6177fa57c27251f8ae2713aacff2076
supermaniszhou <741963634@qq.com>
2020-07-14 14:27:50 +0800

    ad

---
+++ 开发区_功能点/申请会议室添加字段/文件说明/meeting.sql
@@ -0,0 +1,6 @@
+在meeting_room_app表中添加三个字段，如下：
+alter table meeting_room_app add (SQRDH VARCHAR2(50));
+alter table meeting_room_app add (SFYGWHLDCJ VARCHAR2(50));
+alter table meeting_room_app add (HCYQ VARCHAR2(4000));
+
+
