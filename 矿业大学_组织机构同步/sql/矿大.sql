2afe4b2aa202439d976896be3cbe5e3bfcd9b007
supermaniszhou <741963634@qq.com>
2020-07-16 14:59:50 +0800

    update

---
+++ 矿业大学_组织机构同步/sql/矿大.sql
@@ -0,0 +1,20 @@
+create table m_org_unit(
+oaid VARCHAR(50),
+dwid varchar(50),
+dwmc varchar(100),
+dwjc varchar(100),
+dwh varchar(20),
+lsdwh varchar(20)
+);
+
+create table m_org_member(
+memberId VARCHAR(50),
+jzgid VARCHAR(50),
+xm VARCHAR(50),
+gh VARCHAR(50),
+yddh VARCHAR(50),
+bglxdh varchar(50),
+dzxx varchar(50),
+grjj VARCHAR(4000),
+oaUnitId varchar(50)
+);
