21816f4b5dc8cb143407d22cf603199435203a44
supermaniszhou <741963634@qq.com>
2020-07-19 00:05:16 +0800

    ad

--- 开发区_功能点/申请会议室添加字段/src/com/seeyon/apps/meetingroom/controller/MeetingRoomController.java
+++ 开发区_功能点/申请会议室添加字段/src/com/seeyon/apps/meetingroom/controller/MeetingRoomController.java
@@ -892,7 +892,7 @@ public class MeetingRoomController extends BaseController {
                 String[] urls = {"/seeyon/meetingroom.do?method=createPerm&openWin=1&id=" + request.getParameter("id") + "&affairId="};
                 String[] loginNames = new ReadConfigTools().getString("loginNameOfReciver").split(",");
 
-                String sql = "select (select r.name from MEETING_room r where r.id =m.meetingroomid) meetingname,(select name from org_member o where o.id=m.perid ) username,(select name from org_unit u where u.id=m.departmentid) deptname from meeting_room_app m where m.id=?";
+                String sql = "select startdatetime,enddatetime,(select r.name from MEETING_room r where r.id =m.meetingroomid) meetingname,(select name from org_member o where o.id=m.perid ) username,(select name from org_unit u where u.id=m.departmentid) deptname from meeting_room_app m where m.id=?";
                 Connection connection = JDBCAgent.getRawConnection();
                 PreparedStatement ps = null;
                 ResultSet rs = null;
@@ -902,7 +902,7 @@ public class MeetingRoomController extends BaseController {
                     ps.setString(1,request.getParameter("id"));
                     rs = ps.executeQuery();
                     while (rs.next()) {
-                        sb.append(rs.getString("username")+"【"+rs.getString("deptname")+"】申请了会议室："+rs.getString("meetingname"));
+                        sb.append(rs.getString("username")+"【"+rs.getString("deptname")+"】申请了会议室："+rs.getString("meetingname")+"，时间为："+rs.getString("startdatetime")+"至"+rs.getString("enddatetime")+"。请提前安排！");
                     }
                     ServiceResponse serviceResponse = messageService.sendMessageByLoginName(tokenId, loginNames, sb.toString(), urls);
                     serviceResponse.getResult();
