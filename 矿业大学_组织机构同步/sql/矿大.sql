a460651bef5034ead6a5cb06df8479f965f30d66
supermaniszhou <741963634@qq.com>
2020-07-20 16:45:06 +0800

    update

--- 矿业大学_组织机构同步/sql/矿大.sql
+++ 矿业大学_组织机构同步/sql/矿大.sql
@@ -1,14 +1,43 @@
+-- drop table seeyon_oa_dw;
+-- drop table seeyon_oa_jzgjbxx;
+-- drop table m_org_unit;
+-- drop table m_org_member;
+
+create table seeyon_oa_dw(
+dwid varchar(40) primary key not null,
+dwmc varchar(600),
+dwjc varchar(180),
+lsdwh varchar(10),
+dwh varchar(10),
+sfsy varchar(1)
+);
+
+create table seeyon_oa_jzgjbxx(
+zwjbm varchar(20),
+jzgid varchar(40) primary key not null,
+xm varchar(36),
+gh varchar(20),
+dwh varchar(10),
+dzzw varchar(90),
+yddh varchar(30),
+bglxdh varchar(30),
+dzxx varchar(40),
+grjj longtext
+);
+
+
 create table m_org_unit(
-oaid VARCHAR(50),
+oaid VARCHAR(50) primary key not null ,
 dwid varchar(50),
 dwmc varchar(100),
 dwjc varchar(100),
 dwh varchar(20),
-lsdwh varchar(20)
+lsdwh varchar(20),
+sfsy varchar(1)
 );
 
 create table m_org_member(
-memberId VARCHAR(50),
+memberId VARCHAR(50) primary key not null ,
 jzgid VARCHAR(50),
 xm VARCHAR(50),
 gh VARCHAR(50),
