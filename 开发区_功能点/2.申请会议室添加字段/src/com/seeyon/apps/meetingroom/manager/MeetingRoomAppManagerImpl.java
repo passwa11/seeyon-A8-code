5fed3a75cfcb96dd23fb3dec41b926e85383f769
supermaniszhou <741963634@qq.com>
2020-07-31 10:58:07 +0800

    meet

--- 开发区_功能点/2.申请会议室添加字段/src/com/seeyon/apps/meetingroom/manager/MeetingRoomAppManagerImpl.java
+++ 开发区_功能点/2.申请会议室添加字段/src/com/seeyon/apps/meetingroom/manager/MeetingRoomAppManagerImpl.java
@@ -103,8 +103,9 @@ public class MeetingRoomAppManagerImpl implements MeetingRoomAppManager {
         if (null != is && !"".equals(is)) {
             roomApp.setSfygwhldcj(Integer.parseInt(is));
         } else {
-            roomApp.setSfygwhldcj(null);
+//            roomApp.setSfygwhldcj(null);
         }
+
         roomApp.setHcyq(null == appVo.getParameterMap().get("hcyq") ? "" : appVo.getParameterMap().get("hcyq"));
 //		zhou
 
