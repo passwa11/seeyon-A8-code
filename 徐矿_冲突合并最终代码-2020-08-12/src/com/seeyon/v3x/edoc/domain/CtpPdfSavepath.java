e0f58bddcba52d364094256c1b56e554a9100069
supermaniszhou <741963634@qq.com>
2020-08-12 14:40:53 +0800

    合并

---
+++ 徐矿_冲突合并最终代码-2020-08-12/src/com/seeyon/v3x/edoc/domain/CtpPdfSavepath.java
@@ -0,0 +1,35 @@
+package com.seeyon.v3x.edoc.domain;
+
+/**
+ * 周刘成   2019/4/9
+ */
+public class CtpPdfSavepath {
+
+    private Long edocsummaryid;
+    private String savepath;
+
+    public CtpPdfSavepath() {
+    }
+
+    public CtpPdfSavepath(Long edocsummaryid, String savepath) {
+        this.edocsummaryid = edocsummaryid;
+        this.savepath = savepath;
+    }
+
+
+    public Long getEdocsummaryid() {
+        return edocsummaryid;
+    }
+
+    public void setEdocsummaryid(Long edocsummaryid) {
+        this.edocsummaryid = edocsummaryid;
+    }
+
+    public String getSavepath() {
+        return savepath;
+    }
+
+    public void setSavepath(String savepath) {
+        this.savepath = savepath;
+    }
+}
