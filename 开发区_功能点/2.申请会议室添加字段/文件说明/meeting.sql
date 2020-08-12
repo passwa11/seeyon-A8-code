570cbebd16a071a0a3e19d4ed4d85cff8dad536c
supermaniszhou <741963634@qq.com>
2020-08-12 10:26:38 +0800

    meeting

--- 开发区_功能点/2.申请会议室添加字段/文件说明/meeting.sql
+++ 开发区_功能点/2.申请会议室添加字段/文件说明/meeting.sql
@@ -2,4 +2,5 @@
 alter table meeting_room_app add (SQRDH VARCHAR2(50));
 alter table meeting_room_app add (SFYGWHLDCJ number(2));
 alter table meeting_room_app add (HCYQ VARCHAR2(4000));
-
+alter table meeting_room_app add (LDID VARCHAR2(300));
+alter table meeting_room_app add (LDNAME VARCHAR2(300));
