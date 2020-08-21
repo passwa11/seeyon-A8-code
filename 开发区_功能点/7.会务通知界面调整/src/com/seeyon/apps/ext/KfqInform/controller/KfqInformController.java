99cdc5949a08f127f9a41ff780dcc4fc61e064e6
supermaniszhou <741963634@qq.com>
2020-08-21 11:10:41 +0800

    q

--- 开发区_功能点/7.会务通知界面调整/src/com/seeyon/apps/ext/KfqInform/controller/KfqInformController.java
+++ 开发区_功能点/7.会务通知界面调整/src/com/seeyon/apps/ext/KfqInform/controller/KfqInformController.java
@@ -15,16 +15,16 @@ import com.seeyon.ctp.common.controller.BaseController;
 import javax.servlet.http.HttpServletResponse;
 
 import com.seeyon.ctp.util.JDBCAgent;
+import com.seeyon.oainterface.impl.organizationmgr.utils.OrgDepartmentUtils;
 import com.seeyon.v3x.dee.util.SqlExcuteUtil;
 import org.apache.commons.beanutils.BeanUtils;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class KfqInformController extends BaseController {
 
@@ -33,29 +33,134 @@ public class KfqInformController extends BaseController {
 
     public ModelAndView save(HttpServletRequest request, HttpServletResponse response) throws Exception {
         String list = request.getParameter("list");
-        JSONObject json = JSON.parseObject(list);
-        JSONArray jsonArray = (JSONArray) json.get("instance");
-        KfqInform inform = new KfqInform();
-        User user = AppContext.getCurrentUser();
+        if (null != list || !"".equals(list)) {
+            JSONObject json = JSON.parseObject(list);
+            JSONArray jsonArray = (JSONArray) json.get("instance");
+            KfqInform inform = new KfqInform();
+            User user = AppContext.getCurrentUser();
+            List<Map<String, Object>> mapList = getUnitData();
+            Connection connection = JDBCAgent.getRawConnection();
+            try {
+                String memberId = null;
+                if (jsonArray.size() > 0) {
+                    delete(Long.toString(user.getId()));
+                    for (int i = 0; i < jsonArray.size(); i++) {
+                        memberId = ((String) jsonArray.get(i));
+                        if (memberId.contains("Member")) {
+                            inform.setCreateuserid(Long.toString(user.getId()));
+                            inform.setId(System.currentTimeMillis() + i);
+                            inform.setMemberid(memberId.substring(6));
+                            inform.setSort(i + 1);
+                            inform.setMembername(getParentDept(Long.toString(user.getId()), connection, mapList));
+                            informManager.saveInform(inform);
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (null != connection) {
+                    connection.close();
+                }
+            }
+        }
+
+        return null;
+    }
+
+    public String getParentDept(String memberId, Connection connection, List<Map<String, Object>> mapList) {
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        StringBuffer result = new StringBuffer();
+        String sql = "select ORG_DEPARTMENT_ID from ORG_MEMBER where id ='" + memberId + "'";
         try {
-            String memberId = null;
-            if (jsonArray.size() > 0) {
-                delete(Long.toString(user.getId()));
-                for (int i = 0; i < jsonArray.size(); i++) {
-                    memberId = ((String) jsonArray.get(i));
-                    if (memberId.contains("Member")) {
-                        inform.setCreateuserid(Long.toString(user.getId()));
-                        inform.setId(System.currentTimeMillis() + i);
-                        inform.setMemberid(memberId.substring(6));
-                        inform.setSort(i + 1);
-                        informManager.saveInform(inform);
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            String departmentId = "";
+            while (rs.next()) {
+                departmentId = rs.getString(1);
+            }
+
+            for (int i = 0; i < mapList.size(); i++) {
+                if ((mapList.get(i).get("id").toString()).equals(departmentId)) {
+                    String path = (mapList.get(i).get("path")).toString();
+                    String p12 = path.substring(0, 12);
+                    result.append("/");
+                    result.append(getUnitName(p12, mapList));
+                    if (path.length() >= 16) {
+                        String p16 = path.substring(0, 16);
+                        result.append("/");
+                        result.append(getUnitName(p16, mapList));
                     }
                 }
             }
+
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            try {
+                if (null != rs) {
+                    rs.close();
+                }
+                if (null != ps) {
+                    ps.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
-        return null;
+        return result.toString();
+    }
+
+    public String getUnitName(String path, List<Map<String, Object>> mapList) {
+        String name = "";
+        for (int j = 0; j < mapList.size(); j++) {
+            String p = mapList.get(j).get("path").toString();
+            if (p.equals(path)) {
+                name = mapList.get(j).get("name").toString();
+            }
+        }
+        return name;
+    }
+
+
+    public List<Map<String, Object>> getUnitData() {
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Map<String, Object>> mapList = new ArrayList<>();
+        String sql = "select id,name,path from ORG_UNIT";
+        try {
+            connection = JDBCAgent.getRawConnection();
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            Map<String, Object> map = null;
+            while (rs.next()) {
+                map = new HashMap<>();
+                map.put("id", Long.toString(rs.getLong("id")));
+                map.put("name", rs.getString("name"));
+                map.put("path", rs.getString("path"));
+                mapList.add(map);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != rs) {
+                    rs.close();
+                }
+                if (null != ps) {
+                    ps.close();
+                }
+                if (null != connection) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return mapList;
     }
 
     public void delete(String id) {
