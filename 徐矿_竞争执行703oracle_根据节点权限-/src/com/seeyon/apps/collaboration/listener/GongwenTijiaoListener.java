4058a387e24227bbfe5443f7d15b4f4d310f45bf
supermaniszhou <741963634@qq.com>
2020-09-02 11:45:38 +0800

    jie

--- 徐矿_竞争执行703oracle_根据节点权限-/src/com/seeyon/apps/collaboration/listener/GongwenTijiaoListener.java
+++ 徐矿_竞争执行703oracle_根据节点权限-/src/com/seeyon/apps/collaboration/listener/GongwenTijiaoListener.java
@@ -76,7 +76,7 @@ public class GongwenTijiaoListener {
                             }
                         }
                     }
-
+                    tempManager.deleteXkjtTemp(temps);
                 }
             }
         }
