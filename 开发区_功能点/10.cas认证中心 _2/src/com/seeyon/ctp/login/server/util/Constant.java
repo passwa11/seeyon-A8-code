69bc42c5392e17ba1f8339b1264727d763d785ab
supermaniszhou <741963634@qq.com>
2020-09-11 13:35:46 +0800

    login

---
+++ 开发区_功能点/10.cas认证中心 _2/src/com/seeyon/ctp/login/server/util/Constant.java
@@ -0,0 +1,13 @@
+package com.seeyon.ctp.login.server.util;
+/**
+ * @Title:Constant
+ * @Description:常量
+ * @author 张颖辉
+ * @date 2017年9月8日上午10:10:41
+ * @version 1.0
+ */
+public class Constant {
+	//返回成功失败编码
+	public static int CODE_SUCCESS = 0;
+	public static int CODE_FAIL = -1;
+}
---
+++ 开发区_功能点/10.cas认证中心 _2/src/com/seeyon/ctp/login/server/util/TicketUtil.java
@@ -0,0 +1,31 @@
+package com.seeyon.ctp.login.server.util;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Title:TicketUtil
+ * @Description:令牌缓存
+ * @author 张颖辉
+ * @date 2017年9月8日上午10:12:09
+ * @version 1.0
+ */
+public class TicketUtil {
+
+	private static Map<String, String> TICKET_CACHE = new HashMap<>();
+
+	public static void put(String ticket, String account) {
+		TICKET_CACHE.put(ticket, account);
+
+	}
+
+	public static String get(String ticket) {
+		return TICKET_CACHE.get(ticket);
+
+	}
+
+	public static void remove(String ticket) {
+		TICKET_CACHE.remove(ticket);
+
+	}
+}
