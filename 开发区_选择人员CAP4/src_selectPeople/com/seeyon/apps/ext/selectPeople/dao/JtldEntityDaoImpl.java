24bcf33d8ceef0503849f6f25ed414a607dc85d8
supermaniszhou <741963634@qq.com>
2020-09-15 10:33:17 +0800

    add

--- 开发区_选择人员CAP4/src_selectPeople/com/seeyon/apps/ext/selectPeople/dao/JtldEntityDaoImpl.java
+++ 开发区_选择人员CAP4/src_selectPeople/com/seeyon/apps/ext/selectPeople/dao/JtldEntityDaoImpl.java
@@ -2,6 +2,7 @@ package com.seeyon.apps.ext.selectPeople.dao;
 
 import com.seeyon.apps.ext.selectPeople.po.Formson0174;
 import com.seeyon.apps.ext.selectPeople.util.JDBCUtil;
+import com.seeyon.apps.ext.selectPeople.util.KfqContants;
 import com.seeyon.ctp.util.DBAgent;
 
 import java.util.List;
@@ -17,7 +18,13 @@ public class JtldEntityDaoImpl implements JtldEntityDao {
         String sqltest = "select id,field0001 ,field0002,field0003,field0005,field0007,(select showvalue from ctp_enum_item where id =field0005) mval from formmain_0032  order by FIELD0007 asc";
 //        正式
         String sql = "select id,field0001 ,field0002,field0003,field0005,field0007,(select showvalue from ctp_enum_item where id =field0005) mval from formmain_0184  order by FIELD0007 asc";
-        List<Map<String, Object>> list = JDBCUtil.doQuery(sqltest);
+
+        List<Map<String, Object>> list = null;
+        if(KfqContants.DEBUGGER){
+            list = JDBCUtil.doQuery(sqltest);
+        }else{
+            list = JDBCUtil.doQuery(sql);
+        }
         return list;
     }
 
@@ -26,7 +33,12 @@ public class JtldEntityDaoImpl implements JtldEntityDao {
         String sqltest = "select id,field0001 ,field0002,field0003,field0005,field0007,(select showvalue from ctp_enum_item where id =field0005) mval from formmain_0031  order by FIELD0007 asc";
 //        正式
         String sql = "select id,field0001 ,field0002,field0003,field0005,field0007,(select showvalue from ctp_enum_item where id =field0005) mval from formmain_0183  order by FIELD0007 asc";
-        List<Map<String, Object>> list = JDBCUtil.doQuery(sqltest);
+        List<Map<String, Object>> list = null;
+        if(KfqContants.DEBUGGER){
+            list = JDBCUtil.doQuery(sqltest);
+        }else{
+            list = JDBCUtil.doQuery(sql);
+        }
         return list;
     }
 
@@ -35,8 +47,13 @@ public class JtldEntityDaoImpl implements JtldEntityDao {
         String sqltest = "select id,field0001 ,field0002,field0003,field0005,field0007,(select showvalue from ctp_enum_item where id =field0005) mval from formmain_0030  order by FIELD0007 asc";
         //        正式
         String sql = "select id,field0001 ,field0002,field0003,field0005,field0007,(select showvalue from ctp_enum_item where id =field0005) mval from formmain_0175  order by FIELD0007 asc";
-
-        List<Map<String, Object>> list = JDBCUtil.doQuery(sqltest);
+        System.out.println(KfqContants.DEBUGGER);
+        List<Map<String, Object>> list = null;
+        if(KfqContants.DEBUGGER){
+            list = JDBCUtil.doQuery(sqltest);
+        }else{
+            list = JDBCUtil.doQuery(sql);
+        }
         return list;
     }
 
@@ -45,14 +62,24 @@ public class JtldEntityDaoImpl implements JtldEntityDao {
         String sqltest = "select id,field0001 ,field0002,field0003,field0005,field0007,(select showvalue from ctp_enum_item where id =field0005) mval from formmain_0029  order by FIELD0007 asc";
         //        正式
         String sql = "select id,field0001 ,field0002,field0004 field0003,field0005,field0007,(select showvalue from ctp_enum_item where id =field0005) mval from formmain_0174  order by FIELD0007 asc";
-        List<Map<String, Object>> list = JDBCUtil.doQuery(sqltest);
+        List<Map<String, Object>> list = null;
+        if(KfqContants.DEBUGGER){
+            list = JDBCUtil.doQuery(sqltest);
+        }else{
+            list = JDBCUtil.doQuery(sql);
+        }
         return list;
     }
 
     @Override
     public List<Map<String, Object>> selectGonghui(String name) {
         String sql = "select id,field0001 ,field0002,field0003,field0005,field0007,(select showvalue from ctp_enum_item where id =field0005) mval from formmain_0285 order by FIELD0007 asc";
-        List<Map<String, Object>> list = JDBCUtil.doQuery(sql);
+        List<Map<String, Object>> list = null;
+        if(KfqContants.DEBUGGER){
+            list = JDBCUtil.doQuery(sql);
+        }else{
+            list = JDBCUtil.doQuery(sql);
+        }
         return list;
     }
 
