7ad8242ffbecc2d23fa7fcf88c94af7a2943ecd6
supermaniszhou <741963634@qq.com>
2020-09-23 11:36:15 +0800

    推送

---
+++ 医科大学_功能点/OA代办事项推送到金智系统/src/com/seeyon/apps/ext/kypending/event/GovdocOperationEvent.java
@@ -0,0 +1,43 @@
+package com.seeyon.apps.ext.kypending.event;
+
+import com.seeyon.ctp.common.po.affair.CtpAffair;
+import com.seeyon.ctp.event.Event;
+
+import java.util.List;
+
+public class GovdocOperationEvent extends Event {
+
+    private List<CtpAffair> affairs;
+
+    private CtpAffair currentAffair;
+
+    private boolean isStart;
+
+    public GovdocOperationEvent(Object source) {
+        super(source);
+    }
+
+    public List<CtpAffair> getAffairs() {
+        return affairs;
+    }
+
+    public void setAffairs(List<CtpAffair> affairs) {
+        this.affairs = affairs;
+    }
+
+    public CtpAffair getCurrentAffair() {
+        return currentAffair;
+    }
+
+    public void setCurrentAffair(CtpAffair currentAffair) {
+        this.currentAffair = currentAffair;
+    }
+
+    public boolean isStart() {
+        return isStart;
+    }
+
+    public void setStart(boolean start) {
+        isStart = start;
+    }
+}
