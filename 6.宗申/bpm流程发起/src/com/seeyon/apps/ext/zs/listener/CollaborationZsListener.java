c500e70626eeb88bac1b830e091411b64026d145
supermaniszhou <741963634@qq.com>
2020-09-27 15:05:01 +0800

    add

--- 6.宗申/bpm流程发起/src/com/seeyon/apps/ext/zs/listener/CollaborationZsListener.java
+++ 6.宗申/bpm流程发起/src/com/seeyon/apps/ext/zs/listener/CollaborationZsListener.java
@@ -3,9 +3,9 @@ package com.seeyon.apps.ext.zs.listener;
 import com.seeyon.apps.collaboration.event.CollaborationFinishEvent;
 import com.seeyon.apps.collaboration.event.CollaborationStopEvent;
 import com.seeyon.apps.ext.zs.util.JDBCUtil;
-import com.seeyon.ctp.common.po.affair.CtpAffair;
 import com.seeyon.ctp.util.annotation.ListenEvent;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,6 +17,10 @@ public class CollaborationZsListener {
         String sql = "select a.COMPLETE_TIME,(select name from ORG_MEMBER where id= a.MEMBER_ID) memberName,(select OU.name from ORG_UNIT ou,ORG_MEMBER om where OM.id=a.MEMBER_ID and OM.ORG_DEPARTMENT_ID=OU.id) deptName,c.content,case c.ext_att4 when 'disagree' then '不同意' when 'agree' then '同意' when 'haveRead' then '已阅' else '' end result  " +
                 " from CTP_COMMENT_ALL c,CTP_AFFAIR a where MODULE_ID=" + summaryId.longValue() + " and c.AFFAIR_ID=a.id order by a.COMPLETE_TIME asc";
         List<Map<String, Object>> list = JDBCUtil.doQuery(sql);
+        Map<String,Object> map=new HashMap<>();
+        map.put("summaryId",summaryId);
+        map.put("data",list);
+        System.out.println(map);
 
     }
 
@@ -26,6 +30,8 @@ public class CollaborationZsListener {
         String sql = "select a.COMPLETE_TIME,(select name from ORG_MEMBER where id= a.MEMBER_ID) memberName,(select OU.name from ORG_UNIT ou,ORG_MEMBER om where OM.id=a.MEMBER_ID and OM.ORG_DEPARTMENT_ID=OU.id) deptName,c.content,case c.ext_att4 when 'disagree' then '不同意' when 'agree' then '同意' when 'haveRead' then '已阅' else '' end result  " +
                 " from CTP_COMMENT_ALL c,CTP_AFFAIR a where MODULE_ID=" + summaryId.longValue() + " and c.AFFAIR_ID=a.id order by a.COMPLETE_TIME asc";
         List<Map<String, Object>> list = JDBCUtil.doQuery(sql);
+        System.out.println(list);
+
     }
 
 
