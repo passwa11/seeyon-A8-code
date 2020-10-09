862f335abd5fb5e30e2e43eacf5734a1b0295d6f
supermaniszhou <741963634@qq.com>
2020-10-09 14:40:48 +0800

    update

--- 医科大学_单点登录/src/com/seeyon/apps/ext/ssoLogin/sso/SendTest.java
+++ 医科大学_单点登录/src/com/seeyon/apps/ext/ssoLogin/sso/SendTest.java
@@ -19,22 +19,20 @@ public class SendTest {
             String templateId = request.getParameter("templateId");
             String type = request.getParameter("type");
             SSOTicketManager.getInstance().newTicketInfo(ticket, ticket, "xkdxSso");
-//            String url = "/seeyon/collaboration/collaboration.do?method=newColl&from=templateNewColl&templateId=15912627018010";
-//            协同
-//            String url = "/seeyon/collaboration/collaboration.do?method=newColl&from=templateNewColl&templateId="+templateId;
-//            公文
             String url = "";
             if (null != type && !"".equals(type)) {
                 if (type.equals("xt")) {
                     url = "/seeyon/collaboration/collaboration.do?method=newColl&from=templateNewColl&templateId=" + templateId;
                 } else if (type.equals("gw")) {
                     url = "/seeyon/govdoc/govdoc.do?method=newGovdoc&sub_app=1&from=template&templateId=" + templateId;
+                } else if(type.equals("self")){
+                    url ="/seeyon/collaboration/collaboration.do?method=newColl&rescode=F01_newColl&_resourceCode=F01_newColl";
                 }
             }
             String path = "/seeyon/main.do?method=login&ticket=" + ticket + "&login.destination=" + URLEncoder.encode(url.substring(url.indexOf("seeyon") - 1));
             response.sendRedirect(path);
         } catch (IOException e) {
-            log.error("医科系统打开Oa代办事项出错了，错误信息：" + e.getMessage());
+            log.error("医科系统打开Oa模板出错了，错误信息：" + e.getMessage());
         }
     }
 
