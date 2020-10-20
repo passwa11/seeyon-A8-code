a11446a270d4e259e1481113a09f6ee3284beeee
supermaniszhou <741963634@qq.com>
2020-10-20 18:07:54 +0800

    add

--- 6.宗申/bpm流程发起/src/com/seeyon/apps/ext/zs/listener/CollaborationZsListener.java
+++ 6.宗申/bpm流程发起/src/com/seeyon/apps/ext/zs/listener/CollaborationZsListener.java
@@ -3,6 +3,9 @@ package com.seeyon.apps.ext.zs.listener;
 import com.seeyon.apps.collaboration.event.CollaborationAffairsAssignedEvent;
 import com.seeyon.apps.collaboration.event.CollaborationFinishEvent;
 import com.seeyon.apps.collaboration.event.CollaborationStopEvent;
+import com.seeyon.apps.ext.zs.manager.ZsTempFormCorrelationManager;
+import com.seeyon.apps.ext.zs.manager.ZsTempFormCorrelationManagerImpl;
+import com.seeyon.apps.ext.zs.po.ZsTempFormCorrelation;
 import com.seeyon.apps.ext.zs.util.JDBCUtil;
 import com.seeyon.ctp.common.exceptions.BusinessException;
 import com.seeyon.ctp.common.po.affair.CtpAffair;
@@ -23,6 +26,7 @@ import java.io.UnsupportedEncodingException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -32,14 +36,34 @@ public class CollaborationZsListener {
 
     @ListenEvent(event = CollaborationFinishEvent.class, async = true)
     public void finish(CollaborationFinishEvent event) {
-        Long summaryId = event.getSummaryId();
-        String sql = "select a.COMPLETE_TIME,(select name from ORG_MEMBER where id= a.MEMBER_ID) memberName,(select OU.name from ORG_UNIT ou,ORG_MEMBER om where OM.id=a.MEMBER_ID and OM.ORG_DEPARTMENT_ID=OU.id) deptName,c.content,case c.ext_att4 when 'disagree' then '不同意' when 'agree' then '同意' when 'haveRead' then '已阅' else '' end result  " +
-                " from CTP_COMMENT_ALL c,CTP_AFFAIR a where MODULE_ID=" + summaryId.longValue() + " and c.AFFAIR_ID=a.id order by a.COMPLETE_TIME asc";
-        List<Map<String, Object>> list = JDBCUtil.doQuery(sql);
-        Map<String, Object> map = new HashMap<>();
-        map.put("summaryId", summaryId);
-        map.put("data", list);
-        System.out.println(map);
+        CtpAffair affair = event.getAffair();
+        String sql = "select a.COMPLETE_TIME,(select p.login_name from ORG_MEMBER m,ORG_PRINCIPAL p where m.id=p.member_id and m.id= a.MEMBER_ID) loginName,(select name from ORG_MEMBER where id= a.MEMBER_ID) memberName,(select OU.name from ORG_UNIT ou,ORG_MEMBER om where OM.id=a.MEMBER_ID and OM.ORG_DEPARTMENT_ID=OU.id) deptName,c.content,case c.ext_att4 when 'disagree' then '不同意' when 'agree' then '同意' when 'haveRead' then '已阅' else '' end result  " +
+                " from CTP_COMMENT_ALL c,CTP_AFFAIR a where  c.AFFAIR_ID=a.id and c.AFFAIR_ID=" + affair.getId() + " order by a.COMPLETE_TIME asc";
+        Connection connection = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            connection = JDBCAgent.getRawConnection();
+            ps = connection.prepareStatement(sql);
+            rs = ps.executeQuery();
+            toPushInfo(rs, affair.getObjectId().longValue());
+        } catch (Exception e) {
+
+        }finally {
+            try {
+                if (null != rs) {
+                    rs.close();
+                }
+                if (null != ps) {
+                    ps.close();
+                }
+                if (null != connection) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
@@ -54,10 +78,6 @@ public class CollaborationZsListener {
         CtpAffair currentAffair = event.getCurrentAffair();
         String sql = "select a.COMPLETE_TIME,(select name from ORG_MEMBER where id= a.MEMBER_ID) memberName,(select OU.name from ORG_UNIT ou,ORG_MEMBER om where OM.id=a.MEMBER_ID and OM.ORG_DEPARTMENT_ID=OU.id) deptName,c.content,case c.ext_att4 when 'disagree' then '不同意' when 'agree' then '同意' when 'haveRead' then '已阅' else '' end result  " +
                 " from CTP_COMMENT_ALL c,CTP_AFFAIR a where  c.AFFAIR_ID=a.id and c.AFFAIR_ID=" + currentAffair.getId() + " order by a.COMPLETE_TIME asc";
-        String url = "http://192.168.1.88:8088/SrmCommSrv.asmx/upVender";
-        CloseableHttpClient client = HttpClients.createDefault();
-        HttpPost post = new HttpPost(url);
-        List<NameValuePair> pairs = new ArrayList<>();
 
         Connection connection = null;
         PreparedStatement ps = null;
@@ -66,39 +86,76 @@ public class CollaborationZsListener {
             connection = JDBCAgent.getRawConnection();
             ps = connection.prepareStatement(sql);
             rs = ps.executeQuery();
+            boolean flag = false;
             while (rs.next()) {
-                pairs.add(new BasicNameValuePair("FormID", currentAffair.getObjectId().longValue() + ""));
-                pairs.add(new BasicNameValuePair("NodeID", "09"));
-                pairs.add(new BasicNameValuePair("Statue", rs.getString("result")));
-                pairs.add(new BasicNameValuePair("USER", rs.getString("memberName")));
-                pairs.add(new BasicNameValuePair("USERNAME", rs.getString("deptName")));
+                String result = rs.getString("result");
+                if ("不同意".equals(result)) {
+                    flag = true;
+                }
             }
 
-            UrlEncodedFormEntity encodedFormEntity = null;
-            try {
-                encodedFormEntity = new UrlEncodedFormEntity(pairs, "utf-8");
-            } catch (UnsupportedEncodingException e) {
-                e.printStackTrace();
-            }
-            post.setEntity(encodedFormEntity);
-            HttpResponse response = null;
-            try {
-                response = client.execute(post);
-                response.setHeader("Cache-Control", "no-cache");
-                int code = response.getStatusLine().getStatusCode();
-                System.out.println(code);
-                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                    String result = EntityUtils.toString(response.getEntity(), "utf-8");
-                    System.out.println(result);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
+            if (flag) {
+                toPushInfo(rs, currentAffair.getObjectId().longValue());
             }
         } catch (Exception e) {
+        } finally {
+            try {
+                if (null != rs) {
+                    rs.close();
+                }
+                if (null != ps) {
+                    ps.close();
+                }
+                if (null != connection) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
+    public void toPushInfo(ResultSet rs, long summaryId) throws SQLException {
+        String url = "http://192.168.1.88:8088/SrmCommSrv.asmx/upVender";
+        CloseableHttpClient client = HttpClients.createDefault();
+        HttpPost post = new HttpPost(url);
+        List<NameValuePair> pairs = new ArrayList<>();
+
+        ZsTempFormCorrelationManager zsManager = new ZsTempFormCorrelationManagerImpl();
+        Map<String, Object> map = new HashMap<>();
+        map.put("oaSummaryId", summaryId + "");
+        List<ZsTempFormCorrelation> list = zsManager.getFormInfoBySummaryId(map);
+
+        while (rs.next()) {
+            pairs.add(new BasicNameValuePair("FormID", summaryId + ""));
+            pairs.add(new BasicNameValuePair("NodeID", "1"));
+            pairs.add(new BasicNameValuePair("Statue", rs.getString("result")));
+            pairs.add(new BasicNameValuePair("User", rs.getString("loginName")));
+            pairs.add(new BasicNameValuePair("UserName", rs.getString("memberName")));
+            pairs.add(new BasicNameValuePair("Approval", rs.getString("content")));
+            pairs.add(new BasicNameValuePair("SRMID", list.get(0).getThirdId()));
         }
 
-
+        UrlEncodedFormEntity encodedFormEntity = null;
+        try {
+            encodedFormEntity = new UrlEncodedFormEntity(pairs, "utf-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        post.setEntity(encodedFormEntity);
+        HttpResponse response = null;
+        try {
+            response = client.execute(post);
+            response.setHeader("Cache-Control", "no-cache");
+            int code = response.getStatusLine().getStatusCode();
+            System.out.println(code);
+            if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                String result = EntityUtils.toString(response.getEntity(), "utf-8");
+                System.out.println(result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
