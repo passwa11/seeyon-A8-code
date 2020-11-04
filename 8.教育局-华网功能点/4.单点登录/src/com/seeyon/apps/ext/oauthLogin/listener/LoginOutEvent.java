37b543e7fc3ade767d5040250174a36016235923
supermaniszhou <741963634@qq.com>
2020-11-04 09:09:46 +0800

    集成认证中心

--- 8.教育局-华网功能点/4.单点登录/src/com/seeyon/apps/ext/oauthLogin/listener/LoginOutEvent.java
+++ 8.教育局-华网功能点/4.单点登录/src/com/seeyon/apps/ext/oauthLogin/listener/LoginOutEvent.java
@@ -15,19 +15,5 @@ public class LoginOutEvent {
 
     @ListenEvent(event = UserLogoutEvent.class, async = true)
     public void out(UserLogoutEvent event) throws BusinessException {
-        System.out.println("进来了吗？？？？？？？？");
-        List<UserLogoutEvent.LogoutUser> list = event.getUsers();
-        UserLogoutEvent.LogoutUser user = list.get(0);
-        OnlineUser onlineUser = user.getUser();
-        System.out.println(onlineUser.getLoginName());
-        HttpServletResponse response = (HttpServletResponse) AppContext
-                .getThreadContext(GlobalNames.THREAD_CONTEXT_RESPONSE_KEY);
-        PropUtils propUtils = new PropUtils();
-        try {
-            response.sendRedirect(propUtils.getSSOOAuthLogout() + "&clientId=" + propUtils.getSSOClientId() + "&returnUrl=" + java.net.URLEncoder.encode(propUtils.getSSOClientHomePage(), "utf-8"));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
     }
 }
