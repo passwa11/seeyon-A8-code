3fc9a5e58b9c4ec2e322587a2363a65dcde1357f
supermaniszhou <741963634@qq.com>
2020-11-04 09:11:28 +0800

    集成认证中心

--- 8.教育局-华网功能点/4.单点登录/src/com/seeyon/apps/ext/oauthLogin/listener/LoginOutEvent.java
+++ 8.教育局-华网功能点/4.单点登录/src/com/seeyon/apps/ext/oauthLogin/listener/LoginOutEvent.java
@@ -1,15 +1,9 @@
 package com.seeyon.apps.ext.oauthLogin.listener;
 
-import com.seeyon.apps.ext.oauthLogin.util.PropUtils;
-import com.seeyon.ctp.common.AppContext;
-import com.seeyon.ctp.common.GlobalNames;
 import com.seeyon.ctp.common.exceptions.BusinessException;
 import com.seeyon.ctp.login.event.UserLogoutEvent;
-import com.seeyon.ctp.login.online.OnlineUser;
 import com.seeyon.ctp.util.annotation.ListenEvent;
 
-import javax.servlet.http.HttpServletResponse;
-import java.util.List;
 
 public class LoginOutEvent {
 
