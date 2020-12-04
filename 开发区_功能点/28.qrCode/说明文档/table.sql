823c1d4aed38ba9614345b849e06a0ed2572be0f
supermaniszhou <741963634@qq.com>
2020-12-04 14:00:18 +0800

    qr

---
+++ 开发区_功能点/28.qrCode/说明文档/table.sql
@@ -0,0 +1,11 @@
+create table temp_qr_code_bj(
+	id varchar2(50) primary key,
+	create_date date,
+	file_url VARCHAR2(255),
+	filename VARCHAR2(100),
+	mime_type varchar2(50),
+	create_member_id VARCHAR2(50),
+	bj_p1 VARCHAR2(100),
+	bj_p2 VARCHAR2(100),
+	bj_p3 VARCHAR2(100)
+);
