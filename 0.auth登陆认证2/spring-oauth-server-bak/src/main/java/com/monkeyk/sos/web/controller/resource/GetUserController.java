db0181205032758e625f1246e819713be0ab5caf
supermaniszhou <741963634@qq.com>
2021-03-16 10:45:47 +0800

    update

---
+++ 0.auth登陆认证2/spring-oauth-server-bak/src/main/java/com/monkeyk/sos/web/controller/resource/GetUserController.java
@@ -0,0 +1,34 @@
+package com.monkeyk.sos.web.controller.resource;
+
+import com.monkeyk.sos.domain.user.User;
+import com.monkeyk.sos.service.UserService;
+import com.monkeyk.sos.service.dto.UserJsonDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/userInfo/")
+public class GetUserController {
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping("queryByUsername")
+    @ResponseBody
+    public UserJsonDto queryByUsername(String name, HttpServletRequest request) {
+        User user = userService.findByUsername(name);
+
+        UserJsonDto jsonDto = new UserJsonDto();
+        if (null != user) {
+            jsonDto.setUsername(user.username());
+            jsonDto.setGuid(user.guid());
+            jsonDto.setPhone(user.phone());
+            jsonDto.setRealname(user.realname());
+        }
+        return jsonDto;
+    }
+}
